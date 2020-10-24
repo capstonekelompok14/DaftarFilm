f1c17d028daf2d80af1bf0d77c0af4e6982b038a
imamfathur <imam.krenz98@gmail.com>
2020-10-24 15:32:34 +0700

    Penambahan API themoviedb

--- app/src/main/java/com/daftarfilm/MainActivity.java
+++ app/src/main/java/com/daftarfilm/MainActivity.java
@@ -4,21 +4,61 @@ import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView movieList;
+    private RecyclerView moviesList;
     private MoviesAdapter adapter;
+    private MoviesRepository moviesRepository;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        adapter = new MoviesAdapter();
+        moviesRepository = MoviesRepository.getInstance();
 
-        movieList = findViewById(R.id.movies_list);
-        movieList.setLayoutManager(new LinearLayoutManager(this));
-        movieList.setAdapter(adapter);
+        moviesList = findViewById(R.id.movies_list);
+        moviesList.setLayoutManager(new LinearLayoutManager(this));
+
+
+        getGenres();
+    }
+
+    private void getGenres() {
+        moviesRepository.getGenres(new OnGetGenresCallback() {
+            @Override
+            public void onSuccess(List<Genre> genres) {
+                getMovies(genres);
+            }
+
+            @Override
+            public void onError() {
+                showError();
+            }
+        });
+    }
+
+    private void getMovies(final List<Genre> genres) {
+        moviesRepository.getMovies(new OnGetMoviesCallback() {
+            @Override
+            public void onSuccess(List<Movie> movies) {
+                adapter = new MoviesAdapter(movies, genres);
+                moviesList.setAdapter(adapter);
+            }
+
+            @Override
+            public void onError() {
+                showError();
+            }
+        });
+    }
+
+    private void showError() {
+        Toast.makeText(MainActivity.this, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
     }
 }
